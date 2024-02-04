e59bc6d758f547f5ee0c75e50c801c9b6c4ab072
UrielCarrera <a3520110324@alumno.uttehuacan.edu.mx>
2024-02-04 00:07:37 -0600

    Add @mui/icons-material package and update Home component

--- src/pages/Private/Home.tsx
+++ src/pages/Private/Home.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import { Container, Typography, Button, Card, CardContent, Grid } from '@mui/material';
 import amaranto from '../../assets/amaranto.jpeg';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Card>
-        <Grid container justifyContent="center" alignItems="center">
+        <Grid container justifyContent="center" alignItems="center" spacing={3}>
           <Grid item xs={12} md={6}>
-            <img src={amaranto} alt="Welcome" style={{ width: '100%', height: 'auto' }} className="logo" />
+            <img src={amaranto} alt="Welcome" className="logo" />
           </Grid>
           <Grid item xs={12} md={6}>
             <CardContent>
-              <Typography variant="h1" component="div" gutterBottom>
+              <Typography variant="h3" gutterBottom>
                 ¡Bienvenido!
               </Typography>
 
-              <Typography variant="h2" color="text.secondary">
-              Al  Mundo de Huautli
+              <Typography variant="h4" color="text.secondary">
+                Al Mundo de Huautli
               </Typography>
 
-              <Typography variant="h5" color="text.secondary">
-                ¿Estás listo para jugar?
+              <Typography variant="subtitle1" color="text.secondary">
+                Bienvenido al emocionante mundo de Huautli. ¿Estás listo para jugar y explorar?
               </Typography>
 
-              <Button variant="contained" color="primary" style={{ marginTop: '20px' , width : "70%"}} >
-                Jugar
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<PlayCircleOutlineIcon />}
+                style={{ marginTop: '20px', width: '100%' }}
+              >
+                Comenzar a Jugar
               </Button>
             </CardContent>
           </Grid>
