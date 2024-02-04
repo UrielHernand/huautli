deefc7fe5ce5635f4e77bc1a5d9abc7281f74691
RangedRhyme17 <kevinvalderrama77@gmail.com>
2024-02-03 18:41:11 -0600

    T1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import mapage from './pages/mapage';
 import './App.css'
 
 function App() {
---
+++ src/pages/mapage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const mapage: React.FC = () => {
+  return (
+    <div>
+      <h1>Map Page</h1>
+      <p>Map Page</p>
+    </div>
+  );
+};
+
+export default mapage;
